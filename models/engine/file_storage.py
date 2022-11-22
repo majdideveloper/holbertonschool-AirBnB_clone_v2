4b063544f44c96be4bd0db35349847565f533def
majdi aribi <majdi.developer@gmail.com>
2022-11-22 21:37:23 +0100

    delete obj from fileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,6 @@ class FileStorage:
                     temp[key] = val.to_dict()
             return temp
 
-
         return FileStorage.__objects
 
     def new(self, obj):
@@ -45,23 +44,30 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
         """delete obj if exist in FileStorage.__objects"""
-        if obj in FileStorage.__objects:
-            del FileStorage.__objects[obj]
+        
+       
+        delete_item = False
+        for key, val in FileStorage.__objects.items():
+            if obj.__class__.__name__ + "." +obj.id == key:
+                delete_item = True
+        if delete_item:
+            del FileStorage.__objects[obj.__class__.__name__ + "." +obj.id]
 
-
-
+                
+            
+        
