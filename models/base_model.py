797e30b122af5e602241ef0cddc581ee9a2874ad
majdi aribi <majdi.developer@gmail.com>
2022-11-23 15:08:35 +0100

    DBStorage states and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,16 @@
 import uuid
 from datetime import datetime
 
+from sqlalchemy import Column, Integer, Sequence, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -13,12 +20,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
+            
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
@@ -31,6 +39,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +50,12 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary:
+            del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """Delete the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
+        storage.save()
---
+++ models/engine/db_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines a class to manage db_storage for hbnb clone"""
+import json
+
+
+class DBStorage():
+    """This class manages storage of hbnb models in db_storage"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine 
+        
