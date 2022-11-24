26fba74f665e6b1d5529065a9b213b99f67e7c7f
majdi aribi <majdi.developer@gmail.com>
2022-11-24 18:19:23 +0100

    try to create DBStorage - States and Cities

--- models/place.py
+++ models/place.py
@@ -2,11 +2,16 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.review import Review
+from models.amenity import Amenity
 import models
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
+association_table = Table('place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False),
+)
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -24,6 +29,8 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=False)
     amenity_ids = []
     reviews = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
 
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
         @property
@@ -34,4 +41,18 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     review_list.append(review)
             return review_list
+        @property
+        def amenities(self):
+            """Get a list of all linked Amenities."""
+            amenity_list = []
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, value):
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
+        
 
