c779555e68b95a607e04f94f7b1b67dfae72fd88
majdi aribi <majdi.developer@gmail.com>
2022-11-24 09:38:54 +0100

    try to create DBStorage - States and Cities

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, Sequence, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel, Base):
@@ -10,3 +10,4 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False) 
+    
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class to manage db_storage for hbnb clone"""
-import json
+from sqlalchemy import Column, Integer, Sequence, String, DateTime
+from sqlalchemy import create_engine
+from os import getenv
 
+
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 class DBStorage():
     """This class manages storage of hbnb models in db_storage"""
@@ -9,5 +20,51 @@ class DBStorage():
     __session = None
 
     def __init__(self):
-        self.__engine 
+        """ initialize of anew bd create the engine """
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'
+                                    .format(getenv('HBNB_MYSQL_USER'), 
+                                            getenv('HBNB_MYSQL_PWD'),
+                                            getenv('HBNB_MYSQL_HOST'),
+                                            getenv('HBNB_MYSQL_DB')), 
+                                    pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query on the curret database session all objects of the given class.
+        If cls is None, queries all types of objects.
+        Return:
+            Dict of queried classes in the format <class name>.<obj id> = obj."""
+        if cls == None:
+            objs = self.__session.query(State, City, User, Review, Place, Amenity).all()
+        else:
+            objs = self.__session.query(cls.__class__.__name__)
+        resu = {}
+        for obj in objs:
+            resu[obj.__class__.__name__+ "." + obj.id]= obj
+            
+        return resu
+
+    def new(self, obj):
+        """Add obj to the current database session."""
+        self.__session.add(obj)
         
+    def save(self):
+        """Commit all changes to the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj """
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+
+        session_factory = sessionmaker(bind=self.__engine,expire_on_commit=False)
+        Session = scoped_session(session_factory)
+
+        self.__session = Session()
+
+
+         
