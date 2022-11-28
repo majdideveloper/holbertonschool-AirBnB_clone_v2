bf4dee4d5a7b8761f71ab4e25350ec7f27cbd80f
majdi aribi <majdi.developer@gmail.com>
2022-11-28 10:22:46 +0100

    Write a script that starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return 'C %s' % text.replace("_", " ")
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    return 'Python %s' % text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '%n is a number' % n
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
