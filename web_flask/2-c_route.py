9312b462441de8a34a1bbc52ef135daea36b5db3
majdi aribi <majdi.developer@gmail.com>
2022-11-28 10:14:28 +0100

    Write a script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    return 'C %s' % text.replace("_"," ")
+    return 'C %s' % text.replace("_", " ")
 
 
 if __name__ == '__main__':
