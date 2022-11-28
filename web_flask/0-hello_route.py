0110914fa371e1abc05e1eca10d71023bde078cb
majdi aribi <majdi.developer@gmail.com>
2022-11-28 09:44:52 +0100

    Write a script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return ('Hello HBNB!')
